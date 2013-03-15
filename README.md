22e93972aa82233fb670d7f90baa942c393ac088
reterVision <reterclose@gmail.com>
2013-03-15 16:52:55 +0800

    Append messages to request, make it individual to per request.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Flask-Pigeon
 
 - Flask Messages Framework
 - A simple Flask extension implements the similar function as Django's messages framework.
+- Example included, but very bind to Bootstrap.
 
 Examples
 ============
--- flask_pigeon/pigeon.py
+++ flask_pigeon/pigeon.py
@@ -5,6 +5,7 @@ try:
     from flask import _app_ctx_stack as stack
 except ImportError:
     from flask import _request_ctx_stack as stack
+from flask import request
 
 
 class Messages(object):
@@ -40,22 +41,40 @@ class Pigeon(object):
         if hasattr(app, "context_processor"):
             app.context_processor(self.context_processor)
 
+        if hasattr(app, "before_request"):
+            app.before_request(self.before_request)
+
     def teardown(self, exception):
         pass
 
     def context_processor(self):
-        context_messages = list(self._messages)
-        self._messages = []  # Clear the messages array.
-        return dict(messages=context_messages)
+        return dict(messages=self.get_messages())
+
+    def before_request(self):
+        request._messages = []
+
+    def add_messages(self, message):
+        """Add a message to the request using Pigeon"""
+        if hasattr(request, "_messages"):
+            return request._messages.append(message)
+
+    def get_messages(self):
+        """Returns the messages stored in the request if it exists, otherwise
+        returns an empty list.
+        """
+        if hasattr(request, "_messages"):
+            return request._messages
+        else:
+            return []
 
     def info(self, message):
-        self._messages.append(Messages(Messages.INFO, message))
+        self.add_messages(Messages(Messages.INFO, message))
 
     def error(self, message):
-        self._messages.append(Messages(Messages.ERROR, message))
+        self.add_messages(Messages(Messages.ERROR, message))
 
     def success(self, message):
-        self._messages.append(Messages(Messages.SUCCESS, message))
+        self.add_messages(Messages(Messages.SUCCESS, message))
 
     def warning(self, message):
-        self._messages.append(Messages(Messages.WARNING, message))
+        self.add_messages(Messages(Messages.WARNING, message))
